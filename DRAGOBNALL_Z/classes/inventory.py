6eadb76ed0bbd8d701e3f38f04329acef857a925
Noved <morph8hprom@gmail.com>
2020-10-03 13:08:21 -0500

    Added comments to inventory.py

--- DRAGOBNALL_Z/classes/inventory.py
+++ DRAGOBNALL_Z/classes/inventory.py
@@ -1,6 +1,13 @@
 class Item:
+    """
+    Base class for Item creation.
+    Takes arguments self, name, type, description, and prop
+    """
     def __init__(self, name, type, description, prop):
+        # defines item name
         self.name = name
+        # defines item type (potion, elixir, attack)
         self.type = type
-        self.description = description
+        # defines item description
+        self.description = description        
         self.prop = prop
